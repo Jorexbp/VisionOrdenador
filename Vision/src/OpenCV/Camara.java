12bcdd5d363eadbdd652e7d37f10747d3f57df2b
Jorexbp <jorgebp2004@gmail.com>
2023-11-24 20:18:34 +0100

    Deteccion de caras sacado

--- Vision/src/OpenCV/Camara.java
+++ Vision/src/OpenCV/Camara.java
@@ -27,10 +27,10 @@ public class Camara extends JFrame {
 	private static final long serialVersionUID = 1L;
 	// Pantalla de la camara
 	private JLabel pantallaCamara;
-	private JButton btnCapturar, btnExtremos, btnCamara;
+	private JButton btnCapturar, btnExtremos, btnCamara, btnCaras;
 	private VideoCapture capturaVideo;
 	private Mat imagen;
-	private boolean clicked, camara = true;
+	private boolean clicked, camara = true, soloCaras = false;
 
 	public Camara() {
 		// GUI
@@ -41,7 +41,7 @@ public class Camara extends JFrame {
 		add(pantallaCamara);
 
 		btnCapturar = new JButton("Capturar");
-		btnCapturar.setBounds(250, 480, 120, 40);
+		btnCapturar.setBounds(325, 480, 120, 40);
 		add(btnCapturar);
 
 		btnCapturar.addActionListener(new ActionListener() {
@@ -52,25 +52,37 @@ public class Camara extends JFrame {
 		});
 
 		btnExtremos = new JButton("Ver Extremos");
-		btnExtremos.setBounds(100, 480, 120, 40);
+		btnExtremos.setBounds(175, 480, 120, 40);
 		add(btnExtremos);
 
 		btnExtremos.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				camara = false;
+				soloCaras = false;
+			}
+		});
+		btnCaras = new JButton("Ver Caras");
+		btnCaras.setBounds(0, 480, 120, 40);
+		add(btnCaras);
+
+		btnCaras.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				camara = false;
+				soloCaras = true;
 			}
 		});
 
 		btnCamara = new JButton("Ver Cámara");
-		btnCamara.setBounds(400, 480, 120, 40);
+		btnCamara.setBounds(475, 480, 120, 40);
 		add(btnCamara);
 
 		btnCamara.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				camara = true;
-
+				soloCaras = false;
 			}
 		});
 
@@ -106,7 +118,18 @@ public class Camara extends JFrame {
 
 				icono = new ImageIcon(DeteccionCara.detectarYGuardar(imagen));
 
-			} else {
+			} else if(soloCaras){
+				try {
+					ImageIcon ic =
+							new ImageIcon(DeteccionCara.detecarSoloCara(imagen));
+					icono = ic;
+				}catch (Exception e) {
+					
+				}
+					
+				
+
+			}else {
 				icono = new ImageIcon(Extremos.detectarExtremos(imagen));
 
 			}
--- Vision/src/OpenCV/DeteccionCara.java
+++ Vision/src/OpenCV/DeteccionCara.java
@@ -45,13 +45,14 @@ public class DeteccionCara {
 
 		// Escribir a fichero
 		Rect[] caraArr = caras.toArray();
+		int numeroCaras = 0;
 		for (int i = 0; i < caraArr.length; i++) {
 			// Dibujar
 			Imgproc.rectangle(imagen, caraArr[i], new Scalar(0, 0, 255), 3);
 
 			// Texto al lado del rectangulo
 			String label = "Cara";
-
+			numeroCaras++;
 			// Posicion debajo del rectangulo
 			Point puntoTexto = new Point(caraArr[i].tl().x, caraArr[i].br().y + 30);
 
@@ -59,6 +60,9 @@ public class DeteccionCara {
 			Imgproc.putText(imagen, label, puntoTexto, 2, 1.1, new Scalar(255, 255, 255), 2);
 
 		}
+		String nCaras = "Caras encontradas: " + Integer.toString(numeroCaras);
+		Point puntoTexto = new Point(20, 50);
+		Imgproc.putText(imagen, nCaras, puntoTexto, 2, 1.1, new Scalar(0, 255, 255), 2);
 
 		Imgcodecs.imwrite("detecciones/deteccion.jpg", imagen);
 
@@ -68,6 +72,48 @@ public class DeteccionCara {
 		byte[] datosImagen = buffer.toArray();
 
 		return datosImagen;
+	}
+
+	public static byte[] detecarSoloCara(Mat imagen) {
+		MatOfRect caras = new MatOfRect();
+
+		// Convertir a escala de grises
+		Mat frameGris = new Mat();
+		Imgproc.cvtColor(imagen, frameGris, Imgproc.COLOR_BGR2GRAY);
+
+		// Mejora de contraste para obtener un mejor resultado
+		Imgproc.equalizeHist(frameGris, frameGris);
+
+		int altura = frameGris.height();
+		int tamañoCara = Math.round(altura * 0.2f);
+
+		// Deteccion de caras
+		CascadeClassifier cascadaCara = new CascadeClassifier();
+
+		// Cargar el XML de dataset de caras
+		cascadaCara.load("Datasets/haarcascade_frontalface_alt2.xml");
+		cascadaCara.detectMultiScale(frameGris, caras, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
+				new Size(tamañoCara, tamañoCara), new Size());
+
+		// Crear una nueva imagen con la posición original de la cara
+		Rect[] caraArr = caras.toArray();
+		final MatOfByte buffer = new MatOfByte();
+
+		for (Rect caraRect : caraArr) {
+			// Crear una nueva imagen para cada cara
+			Mat imagenCara = new Mat(imagen, caraRect);
+
+			// Escribir la nueva imagen a fichero
+			Imgcodecs.imwrite("detecciones/cara_detectada_" + System.currentTimeMillis() + ".jpg", imagenCara);
+
+			// Convertir la nueva imagen a bytes
+			Imgcodecs.imencode(".jpg", imagenCara, buffer);
+
+			// Devolver los datos de la nueva imagen
+
+		}
+
+		return buffer.toArray();
 	}
 
 	public static void main(String[] args) {
