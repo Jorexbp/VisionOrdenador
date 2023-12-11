a3f2eeb961379c30d0cd8a61ece34845b0362011
Jorexbp <jorgebp2004@gmail.com>
2023-12-11 16:31:53 +0100

    Metodos basicos BBDD

--- src/BBDD_Postgres/ConexionBBDD.java
+++ src/BBDD_Postgres/ConexionBBDD.java
@@ -1,7 +1,13 @@
 package BBDD_Postgres;
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class ConexionBBDD {
 
@@ -9,11 +15,137 @@ public class ConexionBBDD {
 		Connection con = null;
 		try {
 			Class.forName("org.postgresql.Driver");
-			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/JavaJDBC");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Prueba", "postgres", "admin");
+			if (con == null) {
+				JOptionPane.showMessageDialog(null, "No se pudo conectar a la BBDD");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return con;
 	}
+	
+	public void createTable(Connection conn, String table_name){
+        Statement statement;
+        try{
+            String query="create table "+table_name+"(empid SERIAL,name varchar(200),address varchar(200),primary key(empid));";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Created");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void insert_row(Connection conn,String table_name,String name, String address){
+        Statement statement;
+        try {
+            String query=String.format("insert into %s(name,address) values('%s','%s');",table_name,name,address);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Row Inserted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void read_data(Connection conn, String table_name){
+        Statement statement;
+        ResultSet rs=null;
+        try {
+            String query=String.format("select * from %s",table_name);
+            statement=conn.createStatement();
+            rs=statement.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empid")+" ");
+                System.out.print(rs.getString("name")+" ");
+                System.out.println(rs.getString("Address")+" ");
+            }
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void update_name(Connection conn,String table_name, String old_name,String new_name){
+    	// TODO ESTE METODO NO ACTUALIZA CORRECTAMENTE
+        Statement statement;
+        try {
+            String query=String.format("update %s set name='%s' where name='%s'",table_name,new_name,old_name);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Updated");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void search_by_name(Connection conn, String table_name,String name){
+        Statement statement;
+        ResultSet rs=null;
+        try {
+            String query=String.format("select * from %s where name= '%s'",table_name,name);
+            statement=conn.createStatement();
+            rs=statement.executeQuery(query);
+            while (rs.next()){
+                System.out.print(rs.getString("empid")+" ");
+                System.out.print(rs.getString("name")+" ");
+                System.out.println(rs.getString("address"));
+
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void search_by_id(Connection conn, String table_name,int id){
+        Statement statement;
+        ResultSet rs=null;
+        try {
+            String query=String.format("select * from %s where empid= %s",table_name,id);
+            statement=conn.createStatement();
+            rs=statement.executeQuery(query);
+            while (rs.next()){
+                System.out.print(rs.getString("empid")+" ");
+                System.out.print(rs.getString("name")+" ");
+                System.out.println(rs.getString("address"));
+
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_row_by_name(Connection conn,String table_name, String name){
+        Statement statement;
+        try{
+            String query=String.format("delete from %s where name='%s'",table_name,name);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Deleted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void delete_row_by_id(Connection conn,String table_name, int id){
+        Statement statement;
+        try{
+            String query=String.format("delete from %s where empid= %s",table_name,id);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data Deleted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_table(Connection conn, String table_name){
+        Statement statement;
+        try {
+            String query= String.format("drop table %s",table_name);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Deleted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 
 }
--- src/BBDD_Postgres/Post.java
+++ src/BBDD_Postgres/Post.java
@@ -1,7 +1,19 @@
 package BBDD_Postgres;
 
+import java.sql.Connection;
+
 public class Post {
 	public static void main(String args[]) {
+		ConexionBBDD db = new ConexionBBDD();
+		Connection conn = db.getConexion();
+		db.createTable(conn,"employee");
+        //db.insert_row(conn,"employee","Rajat","India");
+       // db.update_name(conn,"employee","Rahul","Rajat");
+       // db.search_by_name(conn,"employee","Rajat");
+       // db.delete_row_by_name(conn,"employee","Rajat");
+     //   db.delete_row_by_id(conn,"employee",2);
+      //  db.read_data(conn,"employee");
+        db.delete_table(conn,"employee");
 		
 	}
 }
